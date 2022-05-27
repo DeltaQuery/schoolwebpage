e5199c503dbd16edd906c0e3686298e68bc081f8
Delta <cebracho94@gmail.com>
2022-05-27 17:36:12 -0400

    solving links problem v.4

--- components/main-header.js
+++ components/main-header.js
@@ -17,7 +17,7 @@ class MainHeader extends HTMLElement {
                             <a class="nav-link active" aria-current="page" href="">Inicio</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/quienes-somos.html">Quiénes somos</a>
+                            <a class="nav-link" href="./quienes-somos.html">Quiénes somos</a>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
